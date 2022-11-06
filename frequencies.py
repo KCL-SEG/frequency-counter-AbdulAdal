19010f0d8032d2fe374d28fe06589e4012af234b
AbdulAdal <k21007037@kcl.ac.uk>
2022-11-06 15:54:29 +0000

    Implement the frequency counter

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,17 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+import string
+
+
 def frequencies(items):
     frequencies = {}
     # Your code goes here
-    return frequencies
+    dict={}
+    for i in range (len(items)):
+        if(isinstance(items[i],str)==False):
+            items[i]=str(items[i])
+    for number in items:
+        count=items.count(number)
+        dict[number]=count
+    return dict
